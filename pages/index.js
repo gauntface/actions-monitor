26fdf6291e0a6d8f4b11ad2fe84069b81df8c446
Matt Gaunt <matt@gaunt.dev>
2022-10-31 17:32:42 -0700

    Adding next scaffold

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={["drac-bg-black", "drac-text-white"].join(' ')}>
+      <Head>
+        <title>GitHub Action Monitor</title>
+        <meta name="description" content="GitHub Action Monitor" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
